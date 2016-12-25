37aac2f2a77759b28c13e1cf44006e76fafae4dd
H-L <hadrien.lepoutre@gmail.com>
2016-12-25 13:21:04 +0100

    [Gulp] Adding IMG and Fonts processing features, Creating configs{} object

--- README.md
+++ README.md
@@ -23,10 +23,10 @@ I personnaly prefer yarn but you can use npm. Just delete the yarn.lock file and
 rm -rf yarn.lock
 npm install
 ```
-:warning: Be careful thought, all the commands below are using yarn, so be sure you change them if you plan to use npm instead. :warning:
+:warning: Be careful thought, all the commands below are using yarn, so be sure you change them if you plan to use npm instead.
 
 ## Sass
-This repo is using Sass for front-end developping.
+This repo is using Sass for front-end developpement.
 If you want to learn Sass, here are some useful links :
 
 * [The official Sass documentation](http://sass-lang.com/)
@@ -36,12 +36,5 @@ If you want to learn Sass, here are some useful links :
 It actually tries to follow the [7-1 pattern](https://sass-guidelin.es/#the-7-1-pattern) and have already some helpers added.
 
 ## Sanitize.css
-As you would see, this repo uses [Sanitize.css](https://github.com/jonathantneal/sanitize.css/).
-Feel free to change the ```@import "sanitize.css";``` by any other reset such as [Normalize](https://necolas.github.io/normalize.css/) or [Reset.css](http://meyerweb.com/eric/tools/css/reset/).
-You can even use yarn to add/remove it :smile:
-
-e.g. :
-```bash
-yarn remove sanitize.css
-yarn add normalize
-```
+As you would see, this repo uses both [Sanitize.css](https://github.com/jonathantneal/sanitize.css/) and [Normalize.css](https://necolas.github.io/normalize.css/).
+Feel free to change the ```@import "sanitize.css";``` and/or ```@import "normalize.css";``` by any other reset such as  or [Reset.css](http://meyerweb.com/eric/tools/css/reset/) (wich is also included in the packages and in the ```sass/vendors``` repo for now).
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,7 @@
+// ## PLUGINS ##
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')({pattern: '*'}); // use 'gulp ls' to get plugins list
+
 // ## PATHS ##
 var basePaths = {
   src : 'src/app/',
@@ -6,7 +10,7 @@ var basePaths = {
 
 var paths = {
   img:{
-    src : basePaths.src + 'img/**/*.{png, svg, jpg}',
+    src : basePaths.src + 'img/**/*.{jpg, png, svg, gif}',
     dist: basePaths.dist + 'img-min/'
   },
   scripts:{
@@ -18,14 +22,38 @@ var paths = {
     dist: basePaths.dist + 'css/'
   },
   fonts:{
-    src : basePaths.src + 'fonts/',
+    src : basePaths.src + 'fonts/**/*',
     dist: basePaths.dist + 'fonts/'
   }
 };
 
-// ## PLUGINS ##
-var gulp = require('gulp');
-var plugins = require('gulp-load-plugins')({pattern: '*'}); // e.g. gulp-strip-debug => plugins.stripDebug
+// ## CONFIGS
+// Easier to use
+var reload = plugins.browserSync.reload;
+
+var configs = {
+  postCssProcessors: [
+    require('autoprefixer'),
+    require('postcss-import')
+  ],
+  cleanCss: {
+    options: {
+      debug: false
+    },
+    function: function(details) {
+      if(configs.cleanCss.options.debug){
+          console.log(details.name + ': ' + details.stats.originalSize);
+          console.log(details.name + ': ' + details.stats.minifiedSize);
+          console.log(details.name + ': ' + 'reduced by ' + Math.round(details.stats.efficiency*10000)/100 + '%');
+          // console.log(details.name + ': ' + details.warnings);
+          // console.log(details.name + ': ' + details.errors);
+        }
+    }
+  },
+  browserSync: {
+    baseDir: './src/dist'
+  }
+};
 
 // ## TASKS ##
 // SASS -> CSS Processing
@@ -33,10 +61,11 @@ gulp.task('build-css', function () {
   return gulp.src(paths.styles.src)
     .pipe(plugins.sourcemaps.init())
     .pipe(plugins.sass())
-    .pipe(plugins.postcss([require('autoprefixer'), require('postcss-import')]))
-    .pipe(plugins.cleanCss())
-    .pipe(plugins.sourcemaps.write())
-    .pipe(gulp.dest(paths.styles.dist));
+    .pipe(plugins.postcss(configs.postCssProcessors))
+    .pipe(plugins.cleanCss(configs.cleanCss.options, configs.cleanCss.function))
+    .pipe(plugins.sourcemaps.write('.'))
+    .pipe(gulp.dest(paths.styles.dist))
+    .pipe(reload({ stream: true }));
 });
 
 // JS Processing
@@ -48,20 +77,21 @@ gulp.task('build-js', function () {
     .pipe(plugins.util.env.prod === true ? plugins.uglify() : plugins.util.noop())
     .pipe(plugins.concat('all.js'))
     .pipe(plugins.sourcemaps.write())
-    .pipe(gulp.dest(paths.scripts.dist));
-});
-
-// IMG Processing
-gulp.task('img', function () {
-  return gulp.src(paths.img.src)
-    .pipe(aGulpPlugin())
-    .pipe(gulp.dest(paths.img.dist));
+    .pipe(gulp.dest(paths.scripts.dist))
+    .pipe(reload({ stream: true }));
 });
 
 // Fonts Processing
 gulp.task('fonts', function () {
   return gulp.src(paths.fonts.src)
-    .pipe(gulp.dest(paths.fonts.dist));
+  .pipe(gulp.dest(paths.fonts.dist));
+});
+
+// IMG Processing
+gulp.task('img', function () {
+  return gulp.src(paths.img.src)
+    .pipe(plugins.imagemin())
+    .pipe(gulp.dest(paths.img.dist));
 });
 
 // Logging all gulp & vendor plugins.
@@ -69,7 +99,18 @@ gulp.task('ls', function() {
   return plugins.util.log(plugins);
 });
 
-// // Testing --prod flag
-// gulp.task('default', function () {
-//   console.log('Production mode : ' + !!plugins.util.env.prod);
-// });
+// ## WATCH ##
+// BrowserSync config
+gulp.task('browserSync', function () {
+  plugins.browserSync.init({
+    server:{
+      baseDir: configs.browserSync.baseDir
+    }
+  });
+});
+
+gulp.task('watch', ['browserSync', 'build-css', 'build-js', 'fonts'], function () {
+  gulp.watch(paths.styles.src, ['build-css']);
+  gulp.watch(paths.scripts.src, ['build-js']);
+  gulp.watch(paths.fonts.src, ['fonts']);
+});
